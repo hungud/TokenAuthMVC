8fea2af52274d9c43993810e2ddd50dfbe5d9a30
hungudym <hungudgm@gmail.com>
2017-12-28 18:51:53 +0700

    UPT

---
+++ TokenAuthMVC.Web/Attributes/SoapAuthorizeAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services.Protocols;
+
+namespace TokenAuthMVC.Attributes
+{
+    // Create a SoapExtensionAttribute for the SOAP Extension that can be
+    // applied to an XML Web service method.
+    [AttributeUsage(AttributeTargets.Method)]
+    public class SoapAuthorizeAttribute : SoapExtensionAttribute
+    {
+        public override Type ExtensionType
+        {
+            get { return typeof(TraceExtension); }
+        }
+
+        public override int Priority { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ TokenAuthMVC.Web/Attributes/SoapAuthorizeExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services.Protocols;
+
+namespace TokenAuthMVC.Attributes
+{
+    // Define a SOAP Extension that traces the SOAP request and SOAP
+    // response for the XML Web service method the SOAP extension is
+    // applied to.
+    public class SoapAuthorizeExtension : SoapExtension
+    {
+        public override object GetInitializer(LogicalMethodInfo methodInfo, SoapExtensionAttribute attribute)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object GetInitializer(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Initialize(object initializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ProcessMessage(SoapMessage message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
